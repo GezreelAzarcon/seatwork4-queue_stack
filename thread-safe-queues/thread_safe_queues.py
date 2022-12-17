50497102938459afb1547229ae56b4904db76d4c
GezreelAzarcon <gezreelwazarcon@gmail.com>
2022-12-17 09:12:11 +0800

    thread safe queues done, not 100% sure how it works

--- thread-safe-queues/thread_safe_queues.py
+++ thread-safe-queues/thread_safe_queues.py
@@ -1,45 +1,25 @@
 import argparse
-from queue import LifoQueue, PriorityQueue, Queue
 import threading
-from random import randint
-from time import sleep
-from random import choice, randint
-
-
+from dataclasses import dataclass, field
+from enum import IntEnum
 from itertools import zip_longest
+from queue import LifoQueue, PriorityQueue, Queue
+from random import choice, randint
+from time import sleep
 from rich.align import Align
 from rich.columns import Columns
 from rich.console import Group
 from rich.live import Live
 from rich.panel import Panel
-from dataclasses import dataclass, field
-from enum import IntEnum
-
-
 
 QUEUE_TYPES = {
-    "fifo": Queue,
-    "lifo": LifoQueue,
+    "fifo": Queue, 
+    "lifo": LifoQueue, 
     "heap": PriorityQueue
 }
 
-def main(args):
-    buffer = QUEUE_TYPES[args.queue]()
+#========================================================================================================================================================
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
-    parser.add_argument("-p", "--producers", type=int, default=3)
-    parser.add_argument("-c", "--consumers", type=int, default=2)
-    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
-    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    try:
-        main(parse_args())
-    except KeyboardInterrupt:
-        pass
 
 PRODUCTS = (
     ":balloon:",
@@ -59,6 +39,36 @@ PRODUCTS = (
     ":yo-yo:",
 )
 
+#========================================================================================================================================================
+
+
+@dataclass(order=True)
+class Product:
+    priority: int
+    label: str = field(compare=False)
+
+    def __str__(self):
+        return self.label
+
+#========================================================================================================================================================
+
+class Priority(IntEnum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+#========================================================================================================================================================
+
+
+PRIORITIZED_PRODUCTS = (
+    Product(Priority.HIGH, ":1st_place_medal:"),
+    Product(Priority.MEDIUM, ":2nd_place_medal:"),
+    Product(Priority.LOW, ":3rd_place_medal:"),
+)
+
+#========================================================================================================================================================
+
+
 class Worker(threading.Thread):
     def __init__(self, speed, buffer):
         super().__init__(daemon=True)
@@ -67,9 +77,6 @@ class Worker(threading.Thread):
         self.product = None
         self.working = False
         self.progress = 0
-
-class Worker(threading.Thread):
-    # ...
 
     @property
     def state(self):
@@ -91,6 +98,32 @@ class Worker(threading.Thread):
             sleep(delay / 100)
             self.progress += 1
 
+#========================================================================================================================================================
+
+class Producer(Worker):
+    def __init__(self, speed, buffer, products):
+        super().__init__(speed, buffer)
+        self.products = products
+
+    def run(self):
+        while True:
+            self.product = choice(self.products)
+            self.simulate_work()
+            self.buffer.put(self.product)
+            self.simulate_idle()
+
+#========================================================================================================================================================
+
+class Consumer(Worker):
+    def run(self):
+        while True:
+            self.product = self.buffer.get()
+            self.simulate_work()
+            self.buffer.task_done()
+            self.simulate_idle()
+
+#========================================================================================================================================================
+
 class View:
     def __init__(self, buffer, producers, consumers):
         self.buffer = buffer
@@ -98,9 +131,7 @@ class View:
         self.consumers = consumers
 
     def animate(self):
-        with Live(
-            self.render(), screen=True, refresh_per_second=10
-        ) as live:
+        with Live(self.render(), screen=True, refresh_per_second=10) as live:
             while True:
                 live.update(self.render())
 
@@ -119,9 +150,7 @@ class View:
             case _:
                 title = products = ""
 
-        rows = [
-            Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)
-        ]
+        rows = [Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)]
         pairs = zip_longest(self.producers, self.consumers)
         for i, (producer, consumer) in enumerate(pairs, 1):
             left_panel = self.panel(producer, f"Producer {i}")
@@ -133,35 +162,17 @@ class View:
         if worker is None:
             return ""
         padding = " " * int(29 / 100 * worker.progress)
-        align = Align(
-            padding + worker.state, align="left", vertical="middle"
-        )
+        align = Align(padding + worker.state, align="left", vertical="middle")
         return Panel(align, height=5, title=title)
 
-class Producer(Worker):
-    def __init__(self, speed, buffer, products):
-        super().__init__(speed, buffer)
-        self.products = products
+#========================================================================================================================================================
 
-    def run(self):
-        while True:
-            self.product = choice(self.products)
-            self.simulate_work()
-            self.buffer.put(self.product)
-            self.simulate_idle()
-
-class Consumer(Worker):
-    def run(self):
-        while True:
-            self.product = self.buffer.get()
-            self.simulate_work()
-            self.buffer.task_done()
-            self.simulate_idle()
 
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
+    products = PRIORITIZED_PRODUCTS if args.queue == "heap" else PRODUCTS
     producers = [
-        Producer(args.producer_speed, buffer, PRODUCTS)
+        Producer(args.producer_speed, buffer, products)
         for _ in range(args.producers)
     ]
     consumers = [
@@ -177,30 +188,23 @@ def main(args):
     view = View(buffer, producers, consumers)
     view.animate()
 
-def main(args):
-    buffer = QUEUE_TYPES[args.queue]()
-    products = PRIORITIZED_PRODUCTS if args.queue == "heap" else PRODUCTS
-    producers = [
-        Producer(args.producer_speed, buffer, products)
-        for _ in range(args.producers)
-    ]
+#========================================================================================================================================================
 
-@dataclass(order=True)
-class Product:
-    priority: int
-    label: str = field(compare=False)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
+    parser.add_argument("-p", "--producers", type=int, default=3)
+    parser.add_argument("-c", "--consumers", type=int, default=2)
+    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
+    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
+    return parser.parse_args()
 
-    def __str__(self):
-        return self.label
+#========================================================================================================================================================
 
-class Priority(IntEnum):
-    HIGH = 1
-    MEDIUM = 2
-    LOW = 3
+if __name__ == "__main__":
+    try:
+        main(parse_args())
+    except KeyboardInterrupt:
+        pass
 
-PRIORITIZED_PRODUCTS = (
-    Product(Priority.HIGH, ":1st_place_medal:"),
-    Product(Priority.MEDIUM, ":2nd_place_medal:"),
-    Product(Priority.LOW, ":3rd_place_medal:"),
-)
-
+#========================================================================================================================================================
